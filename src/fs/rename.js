94c9806c64fb22f20649b4de5403f07ac8ee38a0
Romanuch <bereslavskiy0@gmail.com>
2025-04-23 14:13:13 +0300

    1

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import * as fs from 'fs';
+import { readdir } from 'node:fs/promises';
 const rename = async () => {
     // Write your code here 
+    let oldFileName = 'src/fs/files/wrongFilename.txt';
+    let newFileName = 'src/fs/files/properFilename.md';
+    function filesContentCallback(err, data) {
+        if (err) throw err;
+    }
+    let renamed = (await readdir('src/fs/files', filesContentCallback)).filter((file) => file == 'properFilename.md');
+    function renameCallback(err, data) {
+        if(err) throw new Error('FS operation failed');
+    }
+    if (renamed.length == 0){
+        fs.rename(oldFileName, newFileName, renameCallback);
+    } else throw new Error('FS operation failed');
+    
 };
 
 await rename();
